624bca4e41db7edafc5c344b32a651178227a12c
Dean Hildebrand <dean.hildebrand88@gmail.com>
2026-07-21 15:51:46 +0200

    resolve ci actions, update ruby version

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,14 +1,14 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-require 'spec_helper'
-require 'capybara/cuprite'
-ENV['RAILS_ENV'] ||= 'test'
-require_relative '../config/environment'
+require "spec_helper"
+require "capybara/cuprite"
+ENV["RAILS_ENV"] ||= "test"
+require_relative "../config/environment"
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 # Uncomment the line below in case you have `--require rails_helper` in the `.rspec` file
 # that will avoid rails generators crashing because migrations haven't been run yet
 # return unless Rails.env.test?
-require 'rspec/rails'
+require "rspec/rails"
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -37,7 +37,7 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 
 Capybara.register_driver(:cuprite) do |app|
-  Capybara::Cuprite::Driver.new(app, window_size: [1400, 1400], process_timeout: 20)
+  Capybara::Cuprite::Driver.new(app, window_size: [ 1400, 1400 ], process_timeout: 20)
 end
 Capybara.javascript_driver = :cuprite
 Capybara.default_max_wait_time = 5
@@ -48,7 +48,7 @@ RSpec.configure do |config|
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_paths = [
-    Rails.root.join('spec/fixtures')
+    Rails.root.join("spec/fixtures")
   ]
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
