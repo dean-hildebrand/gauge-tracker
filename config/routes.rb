fe11f60aa7b37d23eb486241ab585d4b040e6002
Dean Hildebrand <dean.hildebrand88@gmail.com>
2026-07-22 21:22:02 +0200

    feat: model approval methods/count/immutability logic - set up turbo stream to replace row with approval & show approval count

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 
   devise_for :users, skip: [ :registrations ]
   resources :gauges, only: [ :index, :new, :create, :show, :edit, :update ] do
-    resources :readings, only: [ :new, :create, :edit, :update ], shallow: true
+    resources :readings, only: [ :new, :create, :edit, :update ], shallow: true do
+      member { patch :approve }
+    end
   end
 
   root "gauges#index"
